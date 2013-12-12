1d81b10a7f3fcda8a95fd155cfd2930939b13a4a
Tero Piirainen <tipiirai@gmail.com>
2013-12-12 10:07:12 +0200

    first commit

---
+++ node/server.js
@@ -0,0 +1,32 @@
+
+var riot = require("./riot"), // https://moot.it/riotjs/
+  template = require('fs').readFileSync('./template.html', "utf-8");
+
+function BASE64(str) {
+  return (new Buffer(str)).toString('base64');
+}
+
+function SHA1(str) {
+  var sum = require('crypto').createHash('sha1');
+  sum.update(str);
+  return sum.digest('hex');
+}
+
+
+require('http').createServer(function(request, response) {
+
+  var data = {
+    title: "Private forum",
+    key: "testapikey",
+    timestamp: Math.round(+new Date / 1000),
+    message: BASE64(JSON.stringify({}))
+  };
+
+  data.signature = SHA1('testapisecretkey' + ' ' + data.message + ' ' + data.timestamp);
+
+  response.writeHeader(200, { "Content-Type": "text/html" });
+  response.write(riot.render(template, data));
+  response.end();
+
+}).listen(8022);
+
