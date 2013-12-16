3bced4bf3755fcad52f82412732452e43e1b7a44
Tero Piirainen <tipiirai@gmail.com>
2013-12-16 19:28:16 +0200

    Node server to handle signed config, SSO (logged / non-logged), forums and commenting (flat & threaded)

---
+++ node/minimal.js
@@ -0,0 +1,32 @@
+
+/* A minimal signed configuration */
+
+var riot = require("./riot"), // moot.it/riotjs
+  template = require('fs').readFileSync(__dirname + '/template.html', "utf-8");
+
+function BASE64(str) {
+  return (new Buffer(str)).toString('base64');
+}
+
+function SHA1(str) {
+  var sum = require('crypto').createHash('sha1');
+  sum.update(str);
+  return sum.digest('hex');
+}
+
+require('http').createServer(function(req, res) {
+
+  var data = {
+    timestamp: Math.round(+new Date / 1000),
+    message: BASE64(JSON.stringify({})),
+    key: "testapikey"
+  };
+
+  data.signature = SHA1('testapisecretkey' + ' ' + data.message + ' ' + data.timestamp);
+
+  res.writeHeader(200, { "Content-Type": "text/html" });
+  res.write(riot.render(template, data));
+  res.end();
+
+}).listen(8080);
+
--- node/server.js
+++ node/server.js
@@ -1,7 +1,17 @@
 
-var riot = require("./riot"), // https://moot.it/riotjs/
-  template = require('fs').readFileSync(__dirname + '/template.html', "utf-8");
+var url = require('url'),
+    riot = require("./riot"), // moot.it/riotjs
+    template = require('fs').readFileSync(__dirname + '/template.html', "utf-8"),
 
+  test_user = {
+    id: "johndoe",
+    avatar: '//cloudinary-a.akamaihd.net/moot/image/upload/t_d1-avatar/v1365823782/tipiirai.jpg',
+    email: "johndoe@acme.org",
+    displayname: "John Doe",
+    is_admin: false
+  };
+
+// required cryptographic functions
 function BASE64(str) {
   return (new Buffer(str)).toString('base64');
 }
@@ -12,21 +22,35 @@ function SHA1(str) {
   return sum.digest('hex');
 }
 
+// convenience method
+String.prototype.has = function(str) {
+  return this.indexOf(str) != -1;
+}
 
-require('http').createServer(function(request, response) {
+require('http').createServer(function(req, res) {
 
+  var path = url.parse(req.url, true).pathname,
+      message = {};
+
+  // SSO user
+  if (path.has("sso")) message.user = path.has("anon") ? {} : test_user;
+
+  // signed config
   var data = {
-    title: "Private forum",
     key: "testapikey",
     timestamp: Math.round(+new Date / 1000),
-    message: BASE64(JSON.stringify({}))
+    message: BASE64(JSON.stringify(message)),
   };
 
   data.signature = SHA1('testapisecretkey' + ' ' + data.message + ' ' + data.timestamp);
 
-  response.writeHeader(200, { "Content-Type": "text/html" });
-  response.write(riot.render(template, data));
-  response.end();
+  // commenting? (unrelated to SSO / signed config)
+  if (path.has("comment")) data.path = "/comments";
+  if (path.has("flat")) data.path = "/comments:my-key";
+
+  res.writeHeader(200, { "Content-Type": "text/html" });
+  res.write(riot.render(template, data));
+  res.end();
 
 }).listen(8022);
 
