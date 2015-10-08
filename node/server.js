01670e9e0134a328fc3710d8756ccfa3a7477460
Tero Piirainen <tipiirai@gmail.com>
2015-10-08 12:14:33 +0300

    Custom SSO example

--- node/server.js
+++ node/server.js
@@ -1,56 +1,72 @@
 
-var url = require('url'),
-    riot = require("./riot"), // muut.com/riotjs
-    template = require('fs').readFileSync(__dirname + '/template.html', "utf-8"),
+var fs = require('fs'),
+    url = require('url'),
+    riot = require('./riot')
 
-  test_user = {
-    id: "johndoe",
-    avatar: '//cloudinary-a.akamaihd.net/moot/image/upload/t_d1-avatar/v1365823782/tipiirai.jpg',
-    email: "johndoe@acme.org",
-    displayname: "John Doe",
-    is_admin: false
-  };
+var test_user = {
+  id: 'johndoe',
+  avatar: '//cloudinary-a.akamaihd.net/moot/image/upload/t_d1-avatar/v1365823782/tipiirai.jpg',
+  email: 'johndoe@acme.org',
+  displayname: 'John Doe',
+  is_admin: false
+}
+
+function read(filename) {
+  try {
+    return fs.readFileSync(__dirname + '/' + filename, 'utf-8')
+  } catch (ignored) {}
+}
 
 // required cryptographic functions
 function BASE64(str) {
-  return (new Buffer(str)).toString('base64');
+  return (new Buffer(str)).toString('base64')
 }
 
 function SHA1(str) {
-  var sum = require('crypto').createHash('sha1');
-  sum.update(str);
-  return sum.digest('hex');
+  var sum = require('crypto').createHash('sha1')
+  sum.update(str)
+  return sum.digest('hex')
 }
 
 // convenience method
 String.prototype.has = function(str) {
-  return this.indexOf(str) != -1;
+  return this.indexOf(str) != -1
 }
 
 require('http').createServer(function(req, res) {
 
   var path = url.parse(req.url, true).pathname,
-      message = {};
+      html = read('default.html'),
+      message = {}
+
+  // favicon
+  if (path.has('favicon')) return res.end()
 
   // SSO user
-  if (path.has("sso")) message.user = path.has("anon") ? {} : test_user;
+  if (path.has('sso')) message.user = path.has('anon') ? {} : test_user
+
+  // commenting? (unrelated to SSO / signed config)
+  if (path.has('flat')) data.path = '/comments:my-key'
+  else if (path.has('comment')) data.path = '/comments'
+  else if (path.length > 2) {
+    message.user = test_user
+    var str = read(path.slice(1, -1) + '.html')
+    if (str) html = str
+  }
 
   // signed config
   var data = {
-    key: "testapikey",
+    key: 'testapikey',
     timestamp: Math.round(+new Date / 1000),
     message: BASE64(JSON.stringify(message)),
-  };
+  }
 
-  data.signature = SHA1('testapisecretkey' + ' ' + data.message + ' ' + data.timestamp);
+  data.signature = SHA1('testapisecretkey' + ' ' + data.message + ' ' + data.timestamp)
 
-  // commenting? (unrelated to SSO / signed config)
-  if (path.has("comment")) data.path = "/comments";
-  if (path.has("flat")) data.path = "/comments:my-key";
+  res.writeHeader(200, { 'Content-Type': 'text/html' })
 
-  res.writeHeader(200, { "Content-Type": "text/html" });
-  res.write(riot.render(template, data));
-  res.end();
+  res.write(riot.render(html, data))
+  res.end()
 
-}).listen(80);
+}).listen(8080)
 
